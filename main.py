2e8da63a50c6a881b550ceff0eb41cfb1fabbf05
Xx_Viyaan_xX <viyaantheking@gmail.com>
2024-11-19 18:33:45 +0530

    file hndling

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-file=open("C:/Users/HP/Desktop/VS Code/file handling/file handling.txt")
-print(file.read())
-file.close()
+readr=open("C:/Users/HP/Desktop/VS Code/file handling/file handling.txt","r")
+print(readr.read())
+readr.close()
+
+writer=open("C:/Users/HP/Desktop/VS Code/file handling/file handling.txt","w")
+print(writer.write("hello"))
+writer.close()
+
+appendr=open("C:/Users/HP/Desktop/VS Code/file handling/file handling.txt","a")
+print(appendr.write("bye"))
+appendr.close()
