def3a960856ce1538cd27ec239b705a2c0875f09
Xx_Viyaan_xX <viyaantheking@gmail.com>
2024-11-19 18:38:32 +0530

    file handling

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ print(readr.read())
 readr.close()
 
 writer=open("C:/Users/HP/Desktop/VS Code/file handling/file handling.txt","w")
-print(writer.write("hello"))
+print(writer.write("hello\n"))
+print(writer.write("good evening"))
 writer.close()
 
 appendr=open("C:/Users/HP/Desktop/VS Code/file handling/file handling.txt","a")
-print(appendr.write("bye"))
+print(appendr.write("bye\n"))
+print(appendr.write("hi"))
 appendr.close()
+
